3f19d7884c4a3f9b0b5e9e31c86a3dbdad4c0764
parsaiabhi <37465432+parsaiabhi@users.noreply.github.com>
2018-05-16 17:35:08 +0530

    second commit

--- Avail_Sol/src/main/java/com/matrix/model/Person.java
+++ Avail_Sol/src/main/java/com/matrix/model/Person.java
@@ -1,6 +1,65 @@
 package com.matrix.model;
 
+
+/**
+ * @author abhishek
+ *
+ */
+/**
+ * @author abhishek
+ *
+ */
+/**
+ * @author abhishek
+ *
+ */
+/**
+ * @author abhishek
+ *
+ */
 public class Person {
+
+	private String name;
+	private String address;
+	private String zipCode;
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getAddress() {
+		return address;
+	}
+	
+	/**
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	
+	
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", address=" + address + ", zipCode=" + zipCode + "]";
+	}
+	
+	
 
 	
 }
